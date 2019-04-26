f76027587c2f02cee16a9fe76a5411d0efc5a43e
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-27 00:09:38 +0200

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     let embed = new Discord.RichEmbed()
         .setColor('#00ff00')
         .setTitle("Un membre à rejoint le discord !")
-        .setDescription('Bienvenue ' + member.toString() + " sur le discord de **BTT Graphics** !")
+        .setDescription(`Bienvenue ${member.user} sur le discord de **BTT Graphics** !`)
         .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
     member.guild.channels.get('571377026968322058').send(embed)
     member.addRole('571369311231279104')
@@ -16,7 +16,7 @@ module.exports = (client) => {
         let embed = new Discord.RichEmbed()
             .setColor('#ff0000')
             .setTitle("Un membre à quitté le discord !")
-            .setDescription("Aurevoir " + member.toString())
+            .setDescription(`Aurevoir ${member.user} !`)
             .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
         member.guild.channels.get('571369311231279104').send(embed)
      
