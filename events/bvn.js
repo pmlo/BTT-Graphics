83959919355fdc670f8f613ac23930d7b05d82ae
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-28 18:35:55 +0200

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports = (client) => {
+    
+    const serverStats = {
+        guildID: '545154825827123211',
+        totalUsersID: '564862621694820367',
+        memberCountID: '564862623921864714',
+        botCountID: '564862977698693130'
+    }
 
     client.on('guildMemberAdd', member =>{
     let embed = new Discord.RichEmbed()
@@ -10,6 +17,9 @@ module.exports = (client) => {
         .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
     member.guild.channels.get('560909214483087400').send(embed)
     member.addRole("572091157555839163")
+    client.channels.get(serverStats.totalUsersID).setName(`Membres Total : ${member.guild.memberCount}`);
+    client.channels.get(serverStats.memberCountID).setName(`Membres Connectés : ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(serverStats.botCountID).setName(`Nombre de Bot : ${member.guild.members.filter(m => m.user.bot).size}`);
     });
 
     client.on('guildMemberRemove', member =>{
@@ -19,6 +29,9 @@ module.exports = (client) => {
             .setDescription(`Aurevoir ${member.user} !`)
             .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
         member.guild.channels.get('560909214483087400').send(embed)
+    client.channels.get(serverStats.totalUsersID).setName(`Membres Total : ${member.guild.memberCount}`);
+    client.channels.get(serverStats.memberCountID).setName(`Membres Connectés : ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(serverStats.botCountID).setName(`Nombre de Bot : ${member.guild.members.filter(m => m.user.bot).size}`);
      
     });
 };
