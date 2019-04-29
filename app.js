b4351d7604b3599ff0149761d2b0d670862f5586
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-29 14:17:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ bot.on("message", async message => {
         .setTitle("Un membre à rejoint le discord !")
         .setDescription(`Bienvenue ${member.user} sur le discord de **BTT Graphics** !`)
         .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
-    member.guild.channels.get('560909214483087400').send(embed)
+    member.guild.channels.get('572395325571072000').send(embed)
     member.addRole("572091157555839163")
     });
 
@@ -235,5 +235,5 @@ bot.on("message", async message => {
             .setTitle("Un membre à quitté le discord !")
             .setDescription(`Aurevoir ${member.user} !`)
             .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
-        member.guild.channels.get('560909214483087400').send(embed)   
+        member.guild.channels.get('572395325571072000').send(embed)   
     });
