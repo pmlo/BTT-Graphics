6af80e5e6f4885a972964ea5c3a8e0a6a062dd6a
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-29 17:16:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ bot.on("ready", () => {
 
 bot.login(process.env.token);
 
-const channelID = "571375326622646274";
+onst channelID = "553983747809345576";
 
 bot.on('message', message => {
     if(message.content.startsWith(prefix + "sayembed")) {
@@ -71,6 +71,20 @@ bot.on('message', message => {
     });
       bot.channels.get("571375350467264565").send("✅ " + message.author.username + " Viens d'avoir accès au Serveur");
     message.delete();
+    }
+    if(message.content === "!modo") {
+      message.author.createDM().then(channel => {
+          var embed = new Discord.RichEmbed()
+          .setColor('#0090ff')
+          .setTitle(':tools: Voici les commandes modérations !')
+          .addField("!kick <@user>", "Kick l'utilisateur mentionné")
+          .addField("!ban <@user>", "Bannir l'utilisateur mentionné")
+          .addField("!purge nombre", "Supprime le nombre de messages indiqué")
+          .addField("!warn <@user>", "Warn l'utilisateur mentionné")
+          .setTimestamp()
+          channel.send(embed);
+    });
+    message.channel.sendMessage('✅ Commandes envoyés en priver !');
     }
 });
 
