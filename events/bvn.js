528a13aeac2f7c626597733671b38c91c8a22d40
P M L O <48804005+pmlo@users.noreply.github.com>
2019-05-15 09:35:34 +0200

    Update bvn.js

--- events/bvn.js
+++ events/bvn.js
@@ -1,24 +1,22 @@
 const Discord = require('discord.js');
 
 module.exports = (client) => {
-
-client.on('guildMemberAdd', member =>{
-    let embed = new Discord.RichEmbed()
-        .setColor('#02ff03')
-        .setTitle("Un membre à rejoint le discord !")
-        .setDescription(`Bienvenue ${member.user} sur le discord de **BTT Graphics** !`)
-        .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
-    member.guild.channels.get('572395325571072000').send(embed)
-    member.addRole('572091157555839163')
-    });
-    
-    client.on('guildMemberRemove', member =>{
+    client.on('guildMemberAdd', member =>{
         let embed = new Discord.RichEmbed()
-            .setColor('#ff0000')
-            .setTitle("Un membre à quitté le discord !")
-            .setDescription("Aurevoir " + member.user + ' !')
+            .setColor('#02ff03')
+            .setTitle("Un membre à rejoint le discord !")
+            .setDescription(`Bienvenue ${member.user} sur le discord de **BTT Graphics** !`)
             .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
-        member.guild.channels.get('57239532557107200').send(embed)
-     
-    });
+        member.guild.channels.get('572395325571072000').send(embed)
+        member.addRole('572091157555839163')
+      });
+        
+      client.on('guildMemberRemove', member =>{
+            let embed = new Discord.RichEmbed()
+                .setColor('#ff0000')
+                .setTitle("Un membre à quitté le discord !")
+                .setDescription(`Aurevoir ${member.user} !`)
+                .setFooter('Nous sommes maintenant ' + member.guild.memberCount)
+          member.guild.channels.get('572395325571072000').send(embed)
+      });
 };
