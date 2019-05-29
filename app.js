93da248d6fb0bbcfd74bc9906df6e6a6acde8bd2
P M L O <48804005+pmlo@users.noreply.github.com>
2019-05-29 15:33:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -75,12 +75,20 @@ bot.on('message', message => {
     });
     message.channel.sendMessage('✅ Commandes envoyés en priver !');
     }
-     if(message.content === "Graphics") {
-        message.member.addRole("553980596821426200")
-        message.member.removeRole("572091157555839163")
-        message.author.send("✅ Vous avez maintenant accès au serveur")
-        message.delete();
-    }
+         if(message.content === "!fun") {
+            var fun_embed = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle(`:metal: Commande Fun`)
+            .addField("**``!sayembed {Texte}``**", "Posté un Embed")
+            .addField("**``!smoke``**", "Le Bot Fume")
+            .addField("**``!pf``**", "Pile ou Face")
+            .addField("**``!calin``**", "Le Bot te fait un Calin")
+            .addField("**``!choose [choix 1] [choix 2]``**", "Le Bot choisie entre plusieurs choix")
+            .addField("**``!8ball [question]``**", "Pose une Question au Bot")
+            .addField("**``!weather [ville]``**", "Voire la Météo de chez toi")
+            .setTimestamp()
+            message.channel.send(fun_embed);
+        }
 });
 
 bot.on("message", async message => {
